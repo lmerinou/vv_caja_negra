4266ce2d0c271f727236607647d823f434787e4f
Luis Merino <lmerinoulizarna@gmail.com>
2021-11-08 12:31:03 +0100

    añadidos cambios en los tests

--- src/test/java/com/practica/cajanegra/PruebaAddAtPos.java
+++ src/test/java/com/practica/cajanegra/PruebaAddAtPos.java
@@ -1,64 +1,75 @@
 package com.practica.cajanegra;
 
 import com.cajanegra.SingleLinkedListImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Assertions.*;
+
+import java.util.Iterator;
 
 
 class AddAtPosTestEquivalencia {
 
     SingleLinkedListImpl<Object> myList;
+    Iterator<Object> elemLista;
+
 
     @BeforeEach
     void setUp() {
         myList = new SingleLinkedListImpl<>(123, "Hola");
+        elemLista = myList.iterator();
     }
 
     @DisplayName("Insertar en posición válida")
     @Test
     public void posValidaTest() {
-        myList.addAtPos("A", 5);
+        myList.addAtPos("A", 2);
+        Assertions.assertEquals(myList.getAtPos(2),"A");
     }
 
     @DisplayName("Insertar en posición ínválida")
     @Test
     public void posInvalidaTest() {
-        myList.addAtPos("C", -1);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos("C", -1));
     }
 
     @DisplayName("Insertar elemento ínválido inferior")
     @Test
     public void elemInvalidoInferiorTest() {
-        myList.addAtPos("?", 6);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos("?", 6));
+
     }
 
     @DisplayName("Insertar elemento ínválido inferior en pos inválida")
     @Test
     public void elemInvalidoPosInvalidaTest() {
-        myList.addAtPos(">", -2);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos(">", -2));
+
     }
 
     @DisplayName("Insertar elemento ínválido superior")
     @Test
     public void elemInvalidoSuperiorTest() {
-        myList.addAtPos("^", 3);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos("^", 3));
+
     }
 
     @DisplayName("Insertar elemento ínválido superior en pos inválida")
     @Test
     public void elemInvalidoSuperiorPosInvalidaTest() {
-        myList.addAtPos("]", -6);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos("]", -6));
+
     }
 
     @DisplayName("Test parameterizados clases equivalencia")
     @ParameterizedTest
-    @CsvSource(value = {"A:5", "C:-1", "?:6", ">:-2", "^:3", "]:-6"}, delimiter = ':')
+    @CsvSource(value = {"C:-1", "?:6", ">:-2", "^:3", "]:-6"}, delimiter = ':')
     public void elementoTest(Object element, int p) {
-        myList.addAtPos(element, p);
-        System.out.println(myList);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos(element, p));
     }
 
 }
@@ -66,18 +77,40 @@ class AddAtPosTestEquivalencia {
 class AddAtPosValorLimite {
 
     SingleLinkedListImpl<Object> myList;
+    Iterator<Object> elemLista;
 
     @BeforeEach
     void setUp() {
         myList = new SingleLinkedListImpl<>(123, "Hola");
+        elemLista = myList.iterator();
     }
+
+    @DisplayName("Insertar elemento inválido")
+    @Test
+    public void elemInvalidoMenos() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos("@", 3));
+
+    }
+    @DisplayName("Insertar elemento inválido")
+    @Test
+    public void elemInvalidoMas() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> myList.addAtPos("[", 9));
+
+    }
+
 
     @DisplayName("Test parameterizados valor límite")
     @ParameterizedTest
-    @CsvSource(value = {"@:3", "A:4", "B:5", "L:6", "Y:7", "Z:8", "[:9", "C:0", "D:1", "E:2"}, delimiter = ':')
+    @CsvSource(value = {"A:4", "B:5", "L:6", "Y:7", "Z:8", "C:0", "D:1", "E:2"}, delimiter = ':')
     public void elementoTest(Object element, int p) {
-        myList.addAtPos(element, p);
-    }
+        myList.addAtPos(element,p);
+        if(p< myList.size()){
+            Assertions.assertEquals(myList.getAtPos(p),element);
+        }
+        else{
+            Assertions.assertEquals((myList.getAtPos(myList.size())),element);
+        }
 
+    }
 
 }

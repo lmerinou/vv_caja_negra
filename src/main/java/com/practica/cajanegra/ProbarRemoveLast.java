c2fddce9ea46929eb041bb0df9f481859def5b5c
lucas.marco.pardo@alumnos.upm.es <Clevermola555>
2021-11-08 12:52:47 +0100

    Arreglos en RemoveLast

--- src/main/java/com/practica/cajanegra/ProbarRemoveLast.java
+++ src/main/java/com/practica/cajanegra/ProbarRemoveLast.java
@@ -9,23 +9,25 @@ public class ProbarRemoveLast {
         //CASOS DE PRUEBA INICIALES
 
         //PRIMER CASO DE PRUEBA
-        SingleLinkedListImpl<Object> miListaLlena1 = new SingleLinkedListImpl<>("A", "BB", "L");
+        SingleLinkedListImpl<Object> miListaLlena1 = new SingleLinkedListImpl<>();
+        miListaLlena1.addNTimes("A", 122000);
+        System.out.println("Contenido 2: " + miListaLlena1.size());
         miListaLlena1.removeLast();
-        System.out.println("Contenido 1: " + miListaLlena1);
-
+        System.out.println("Contenido 1: " + miListaLlena1.size());
+/*
         //SEGUNDO CASO DE PRUEBA
-        SingleLinkedListImpl<Object> miListaLlena2 = new SingleLinkedListImpl<>("A", "BB", "[");
-        miListaLlena2.removeLast();
-        System.out.println("Contenido 2: " + miListaLlena2);
+        SingleLinkedListImpl<Object> miListaLlena2 = new SingleLinkedListImpl<>();
+        miListaLlena2.addNTimes("A", 1000000);
+        System.out.println("Contenido 2: " + miListaLlena2.size());
+        //miListaLlena2.removeLast();
+        //System.out.println("Contenido 2: " + miListaLlena2.size());
 
         //TERCER CASO DE PRUEBA
-        SingleLinkedListImpl<Object> miListaLlena3 = new SingleLinkedListImpl<>("A", "BB", "?");
-        miListaLlena3.removeLast();
-        System.out.println("Contenido 3: " + miListaLlena3);
-
-        //CUARTO CASO DE PRUEBA
         SingleLinkedListImpl<Object> miListaVacia = new SingleLinkedListImpl<>();
+        System.out.println("Contenido 4: " + miListaVacia.size());
         miListaVacia.removeLast();
-        System.out.println("Contenido 4: " + miListaVacia);
+        System.out.println("Contenido 4: " + miListaVacia.size());
+        */
+
     }
 }
